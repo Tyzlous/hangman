3d65e73043d082f18c481335a6aaba4969b02eda
Tyzlous <rockingrobin.frank@gmail.com>
2018-07-28 13:53:41 +0200

    Added a rand seed.

--- TeamHangman/TeamHangman/Main.cpp
+++ TeamHangman/TeamHangman/Main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <time.h>
 #include <SFML/Graphics.hpp>
 #include "Gamestate.h"
 #include "HWindow.h"
 #include "Hangman.h"
 
 
+
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//typedef Gamestate::State state;
-	
+	srand(time(NULL));
 	Hangman* hangman = new Hangman();
 	hangman->Start();
 
